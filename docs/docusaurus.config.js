d203ab5d251aebff5226e21390eafcdbb7918070
iznyn <mahtunis@gmail.com>
2021-09-26 22:39:15 +0700

    feat(docs): create docs base with docusaurus

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,111 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(
+  module.exports = {
+    title: 'Frontend Toolkit',
+    tagline: 'Frontend Toolkit are cool',
+    url: 'http://frontend-toolkit.arfcodes.com/',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'arfcodes', // Usually your GitHub org/user name.
+    projectName: '@arfcodes/frontend-toolkit', // Usually your repo name.
+
+    presets: [
+      [
+        '@docusaurus/preset-classic',
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: require.resolve('./sidebars.js'),
+            // Please change this to your repo.
+            editUrl:
+              'https://github.com/facebook/docusaurus/edit/main/website/',
+          },
+          tips: {
+            sidebarPath: require.resolve('./sidebars.js'),
+            // Please change this to your repo.
+            editUrl:
+              'https://github.com/facebook/docusaurus/edit/main/website/',
+          },
+          blog: {
+            showReadingTime: true,
+            // Please change this to your repo.
+            editUrl:
+              'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+          },
+          theme: {
+            customCss: require.resolve('./src/css/custom.css'),
+          },
+        }),
+      ],
+    ],
+
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        navbar: {
+          title: 'FE Toolkit',
+          logo: {
+            alt: 'Frontend Toolkit Logo',
+            src: 'img/logo.svg',
+          },
+          items: [
+            {
+              type: 'doc',
+              docId: 'intro',
+              position: 'left',
+              label: 'Docs',
+            },
+            { to: '/blog', label: 'Blog', position: 'left' },
+            {
+              href: 'https://github.com/arfcodes/frontend-toolkit',
+              label: 'GitHub',
+              position: 'right',
+            },
+          ],
+        },
+        footer: {
+          style: 'dark',
+          links: [
+            {
+              title: 'Docs',
+              items: [
+                {
+                  label: 'Introduction',
+                  to: '/docs/intro',
+                },
+              ],
+            },
+            {
+              title: 'Community',
+              items: [
+                {
+                  label: 'Twitter',
+                  href: 'https://twitter.com/iznyn',
+                },
+              ],
+            },
+            {
+              title: 'More',
+              items: [
+                {
+                  label: 'GitHub',
+                  href: 'https://github.com/arfcodes/frontend-toolkit',
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} @arfcodes. Built with Docusaurus.`,
+        },
+        prism: {
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
+        },
+      }),
+  }
+);
